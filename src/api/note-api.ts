beb6135aeacde83e9a8a9254efbe1a5d63a96b2f
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-14 09:10:52 +0100

    store

--- src/api/note-api.ts
+++ src/api/note-api.ts
@@ -1,21 +1,26 @@
 import axios from "axios"
 
 const BASE_URL = "http://localhost:3000/notes"
-
+interface Notes{
+    id:string
+    title:string,
+    content:string,
+    created_at:string
+}
 export class NoteApi{
-    static async create(){
-
+    static async create(note:Notes){
+        return (await axios.post(`${BASE_URL}`,note)).data
     }
     static async fetchAll(){
-
+        return (await axios.get(`${BASE_URL}`)).data
     }
-    static async fetchById(){
-
+    static async fetchById(noteId:number){
+        return (await axios.get(`${BASE_URL}/${noteId}`)).data
     }
-    static async deleteById(){
-
+    static async deleteById(noteId:number){
+        return (await axios.delete(`${BASE_URL}/${noteId}`)).data
     }
-    static async updateById(){
-        
+    static async update(note:Notes){
+        return (await axios.post(`${BASE_URL}/${note.id}`,note)).data
     }
 }
---
+++ src/store/note/note-slice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Note {
+    id:number,
+    title:string,
+    content:string,
+    created_at:string
+}
+
+interface NoteState{
+    noteList:Note[]
+}
+
+const initialState:NoteState = {
+    noteList:[]
+}
+
+export const noteSlice = createSlice({
+    name:"noteSlice",
+    initialState,
+    reducers:{
+        setNoteList:()=>{
+            
+        }
+    }
+})
