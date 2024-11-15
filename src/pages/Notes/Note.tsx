d5bf09846ab200b299792531734f68c3ae8c5f7e
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-15 18:52:36 +0100

    edit

--- src/pages/Notes/Note.tsx
+++ src/pages/Notes/Note.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { RootState } from '../../store'
 import { useSelector } from 'react-redux'
@@ -7,12 +7,14 @@ import NoteForm from '../../components/NoteForm/NoteForm'
 
 
 const Note = () => {
+  const [isEditable,setISEditable] = useState(false)
   const {id} = useParams()
   const note = useSelector((store:RootState)=>store.NOTE.noteList.find(note=>note.id === id))
+  
   console.log(note)
   return (
     <>
-        {note && <NoteForm note={note} isEditable={false} title={note.title} onClickEdit={()=>""} onClickTrash={()=>""}/>}
+        {note && <NoteForm note={note} isEditable={isEditable} title={isEditable ? "Edit note" : note.title} onClickEdit={()=>setISEditable(!isEditable)} onClickTrash={()=>""}/>}
     </>
   )
 }
