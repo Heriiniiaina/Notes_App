ec478bbf3aea78f4072a1ef5553d185f1bf37433
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-15 19:36:03 +0100

    okz

--- src/pages/Notes/Note.tsx
+++ src/pages/Notes/Note.tsx
@@ -4,13 +4,20 @@ import { RootState } from '../../store'
 import { useSelector } from 'react-redux'
 import NoteForm from '../../components/NoteForm/NoteForm'
 
-
+interface FORMVALUE{
+  id:string,
+  title:string,
+  content:string,
+  created_at:string
+}
 
 const Note = () => {
   const [isEditable,setISEditable] = useState(false)
   const {id} = useParams()
   const note = useSelector((store:RootState)=>store.NOTE.noteList.find(note=>note.id === id))
-  async function submit(formValue){}
+  async function submit(formValue:object){
+    
+  }
   console.log(note)
   return (
     <>
