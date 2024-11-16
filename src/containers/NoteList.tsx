f827550a158a7558ca6c7214579c6d0364ccd44c
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-16 14:04:17 +0100

    remove nav

--- src/containers/NoteList.tsx
+++ src/containers/NoteList.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store'
 import TextCard from '../components/TextCard/TextCard'
-import { Navigate, useNavigate } from 'react-router-dom'
+import {  useNavigate } from 'react-router-dom'
 
 
 
