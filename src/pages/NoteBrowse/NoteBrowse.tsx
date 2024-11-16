f5a62ba2b9ffc54c3fd43303f84322258f135ed5
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-16 14:32:30 +0100

    search

--- src/pages/NoteBrowse/NoteBrowse.tsx
+++ src/pages/NoteBrowse/NoteBrowse.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import TextCard from '../../components/TextCard/TextCard'
 import NoteList from '../../containers/NoteList'
+import SearchBar from '../SearchBar/SearchBar'
 
 
 
 const NoteBrowse = () => {
   return (
-    <NoteList />
+    <>
+      <SearchBar/>
+      <NoteList />
+    </>
        
   )
 }
---
+++ src/pages/SearchBar/SearchBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaSearchengin } from 'react-icons/fa6'
+
+
+
+const SearchBar = () => {
+  return (
+    <>
+        <FaSearchengin/>
+        
+    </>
+  )
+}
+
+export default SearchBar
