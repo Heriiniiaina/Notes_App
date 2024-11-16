b95eae50693898d00c7ab071713ddcad6304a42b
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-16 14:03:42 +0100

    fix navigation

--- src/containers/NoteList.tsx
+++ src/containers/NoteList.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store'
 import TextCard from '../components/TextCard/TextCard'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 
 
 const NoteList = () => {
+  const naviagte =useNavigate()
     const noteList = useSelector((store:RootState)=>store.NOTE.noteList)
   return (
     <div className='flex justify-center flex-row w-full flex-wrap'>
@@ -13,7 +15,7 @@ const NoteList = () => {
             noteList.map((note)=>{
                 return <div key={note.id} className='m-4 '>
                     <TextCard title={note.title} subtitle={note.created_at} content={note.content}
-                     onClick={()=>alert("s") } onClickTrash={()=> alert("del")}/>
+                     onClick={()=>naviagte("/note/"+note.id) } onClickTrash={()=> alert("del")}/>
                 </div>
             })
         }
