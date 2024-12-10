8112570e2bddd229c59a801a2189ee478fd125b3
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-10 19:29:34 +0100

    route

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, register, sendResetPasswordCode } from "../controllers/auth.controller.js";
+import { login, register, sendResetPasswordCode, verifyResetPasswordCode } from "../controllers/auth.controller.js";
 
 const router =express.Router()
 
 router.post("/register",register)
 router.post("/login",login)
 router.patch("/sendResetPasswordCode",sendResetPasswordCode)
+router.patch("/verifyResetPasswordCode",verifyResetPasswordCode)
 export default router
